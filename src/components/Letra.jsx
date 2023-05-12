094cd6235f1623cfa773db4a6638e4b4907b71a6
Ana <anaamaria.packer@gmail.com>
2023-05-12 16:05:40 -0300

    ao pressionar uma letra o botão dessa letra clicada fica desabilitado

---
+++ src/components/Letra.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function Letra(props){
+    const letra = props.letra
+    const isDesabilitado = props.isDesabilitado
+    const letrasSelecionadas = props.letrasSelecionadas
+    const setLetrasSelecionadas = props.setLetrasSelecionadas
+    const [isLetraClicada, setIsLetraClicada] = useState(false)
+
+    function escolherLetra(){
+        setLetrasSelecionadas([...letrasSelecionadas, letra])
+        setIsLetraClicada(true)
+        console.log(letrasSelecionadas)
+    }
+
+
+    return(
+        <button onClick={escolherLetra} disabled={isLetraClicada ||isDesabilitado}>{letra}</button>
+    )
+}
--- src/components/Letras.jsx
+++ src/components/Letras.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
+import Letra from "./Letra"
+
 export default function Letras(props) {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     const isDesabilitado = props.isDesabilitado
+    const [letrasSelecionadas, setLetrasSelecionadas] = useState([])
     
     return (
         <div className="letras">
-           {alfabeto.map(letra => <button disabled={isDesabilitado}>{letra}</button>)}
+           {alfabeto.map(letra => <Letra key={letra} letrasSelecionadas={letrasSelecionadas} setLetrasSelecionadas={setLetrasSelecionadas} isDesabilitado={isDesabilitado } letra={letra}/>)}
         </div>
     )
 }
